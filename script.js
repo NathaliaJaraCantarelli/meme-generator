866683c08cebdfa635628b6ea8d7bc9fc63b816b
Nathalia <nathalia.cantarelli@edu.pucrs..br>
2022-08-05 01:26:13 -0300

    Requisito 2 conluido

--- script.js
+++ script.js
@@ -4,3 +4,16 @@ memeTextInput.addEventListener('keyup', function() {
     let textParagraph = document.getElementById('meme-text');
     textParagraph.innerText = memeTextInputValue;
 })
+
+let memeImage = document.getElementById('meme-insert');
+memeImage.addEventListener('change', function(){
+    console.log(memeImage);
+    let container = document.getElementById('meme-image-container');
+    container.style.background = memeImage.value;
+})
+
+function fileUpload (event) {
+    let memeImage = document.getElementById('meme-image');
+    memeImage.src = URL.createObjectURL(event.target.files[0]); //https://stackoverflow.com/questions/33920409/how-to-get-local-filename-using-url-createobjecturl
+
+}
